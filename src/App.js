e5d0fe576f1bfdfcde78b3a527705700e125c88d
jngmnj <exorbitance@naver.com>
2023-11-27 16:27:13 +0900

    [ADD] immer 사용, 함수형 업데이트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import {useCallback, useRef, useState} from "react";
+import { produce } from "immer";
 
 function App() {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({name: '', username: ''});
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null
+  });
+
+  const onChange = useCallback(
+    e => {
+      const {name, value} = e.target;
+      setForm(
+        // {...form,
+        // [name]: [value]}
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
+    },
+    [form]
+  );
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username
+      };
+
+      setData(
+        // {...data,
+        // array: data.array.concat(info)}
+        produce(data, draft => {
+          draft.array.push(info)
+        })
+      );
+
+      setForm({
+        name: '',
+        username: ''
+      });
+      nextId.current += 1;
+    }, 
+    [data, form.name, form.username]
+  );
+  
+  const onRemove = useCallback(
+    id => {
+      setData(
+        // {...data,
+        // array: data.array.filter(info => info.id !== id)}
+        produce(data, draft => {
+          draft.array.splice(draft.array.findIndex((info) => info.id === id), 1)
+        })
+      );
+    },
+    [data]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form-wrapper'>
+      <form onSubmit={onSubmit}> 
+        <input 
+          name="username"
+          placeholder='아이디'
+          value={form.username}
+          onChange={onChange}
+        />
+        <input 
+          name="name"
+          placeholder='이름'
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div className='data-wrapper'>
+        <ul>
+          {data.array.map(info => (
+            <li key={info.id} onClick={() => onRemove(info.id)} >
+              {info.username} {info.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
